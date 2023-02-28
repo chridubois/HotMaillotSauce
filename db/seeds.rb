468e8c69799780be64107e26e892748b15dd39c3
Christophe Dubois <idategto11@gmail.com>
2023-02-28 10:46:33 +0100

    génération de seed pas encore fini, mais ca marche. A peaufiner dans la journée

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "nokogiri"
+
+i = 1
+
+while i < 2
+
+  url = "https://www.vintagefootballarea.com/collections/tous-les-maillots?page=#{i}"
+
+  html_file = URI.open(url).read
+  html_doc = Nokogiri::HTML.parse(html_file)
+
+  # p html_doc.search(".rimage__image").first.attributes["data-src"].value
+
+  html_doc.search(".product-block").each do |element|
+    p "Description #{element.search(".title").text.strip}"
+    p "Année #{element.search(".title").text.strip.split('-').first.strip}"
+    p "Equipe #{element.search(".title").text.strip.split('-')[1].split('(').first.strip}"
+    p "Lien Image #{element.search(".rimage__image").first.attributes["data-src"].value}"
+    p "Price #{element.search(".price").text.strip.gsub('€', '')}"
+
+    split_element = element.search(".title").text.strip.split('-')
+    p split_element
+    if split_element[2]
+      p "Taille #{element.search(".title").text.strip.split('-')[2].split('(')[1].gsub(')', '').strip}"
+      p element.search(".title").text.strip.split('-')[2].split('(')[0].split('#')
+      p "Joueur #{element.search(".title").text.strip.split('-')[2].split('(')[0].split('#')[0].strip}"
+      p "Numéro #{element.search(".title").text.strip.split('-')[2].split('(')[0].split('#')[1].strip}"
+    else
+      p "Taille #{element.search(".title").text.strip.split('-')[1].split('(')[1].gsub(')', '').strip}"
+    end
+    p "---------------"
+  end
+  i += 1
+end
