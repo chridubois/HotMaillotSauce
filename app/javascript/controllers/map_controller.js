18c773b4e0d949a24fed06e7cbdb219a391756cb
Christophe Dubois <idategto11@gmail.com>
2023-03-03 15:50:55 +0100

    geocoding finish

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,21 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  connect() {
-    console.log(`Hello Map !`);
-  }
 
   static values = {
     apiKey: String,
-    markers: Array
+    marker: Object
   }
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v12",
+      center:[ this.markerValue.lng, this.markerValue.lat ], // starting position [lng, lat]
+      zoom: 14 // starting zoom
     })
+    console.log(this.apiKeyValue);
+    console.log(this.markerValue);
+    this.#addMarkerToMap();
+  }
+
+  #addMarkerToMap() {
+    new mapboxgl.Marker()
+      .setLngLat([ this.markerValue.lng, this.markerValue.lat ])
+      .addTo(this.map)
   }
 }
